cea857eebf259c2c2f2794e02828ed03b27272ad
Vladimirs Nordholm <v@vladde.net>
2020-08-03 20:01:22 +0200

    prolly makes it compatible with php 5.3

--- tests/TestEnum.php
+++ tests/TestEnum.php
@@ -1,14 +1,12 @@
 <?php
 
-declare(strict_types=1);
-
 namespace SimpleEnum\Tests;
 
 use SimpleEnum\Enum;
 
 class TestEnum extends Enum
 {
-    public const foo = 1;
-    public const bar = 2;
-    public const baz = 3;
+    const foo = 1;
+    const bar = 2;
+    const baz = 3;
 }
